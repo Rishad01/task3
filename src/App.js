7e9a7e7aad809c33db1a229c46bec8076e122dfd
Rishad01 <rishadhusainrizvi786@gmail.com>
2024-06-24 03:05:19 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-import { Card, Container, Image, Row, Col, Overlay } from "react-bootstrap";
+import { Card, Container, Image, Row, Col, Overlay,Spinner,Button } from "react-bootstrap";
 import axios from 'axios';
 import WOW from 'wowjs';
 import person from "./logo.svg";
+import "./style.css";
 
 function Profile(props) {
   React.useEffect(()=>{
@@ -11,17 +12,20 @@ function Profile(props) {
     wow.init();
 },[]);
   return (
-    <Card onClick={(e) => props.onClick(props.items, e.currentTarget)} className="mb-3 WOW bounceInRight">
+    <Card onClick={(e) => props.onClick(props.items, e.currentTarget)} className="mb-3 profile-card WOW bounceInRight">
       <Card.Body>
         <Container>
-          <Row>
-            <Col xs='auto'>
-              <Image style={{ height: '40px', width: '40px' }} src={props.items.avatar || person} alt='Pic' roundedCircle />
-            </Col>
-            <Col xs='auto'>
-              <p>{props.items.profile.firstName} {props.items.profile.lastName}</p>
-            </Col>
-          </Row>
+        <Row className='align-items-center mb-3'>
+          <Col xs='auto'>
+              <Image style={{ height: '40px', width: '40px' }} src={props.items.avatar} alt='Pic' roundedCircle />
+          </Col>
+          <Col xs='auto'>
+            <div>
+              <p style={{ margin: '0' }}>{props.items.profile.firstName} {props.items.profile.lastName}</p>
+              <p style={{ margin: '0',color:'GrayText' }}>{props.items.jobTitle}</p>
+            </div>
+          </Col>
+         </Row>
         </Container>
       </Card.Body>
     </Card>
@@ -56,7 +60,11 @@ function App() {
   }, []);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (<Container fluid className="d-flex justify-content-center align-items-center vh-100">
+    <Spinner animation="border" role="status">
+    <span className="visually-hidden">Loading...</span>
+  </Spinner>
+  </Container>);
   }
 
   if (error) {
@@ -64,15 +72,34 @@ function App() {
   }
 
   const handleProfileClick = (profile, target) => {
-    setSelectedProfile(profile);
-    setTarget(target);
-    setShowOverlay(true);
+    if (selectedProfile && profile.id === selectedProfile.id) {
+      setSelectedProfile(null);
+      setTarget(null);
+      setShowOverlay(false);
+      target.classList.remove('text-bg-info');
+    } else {
+      setSelectedProfile(profile);
+      setTarget(target);
+      setShowOverlay(true);
+      const previousTarget = document.querySelector('.text-bg-info');
+        if (previousTarget) {
+          previousTarget.classList.remove('text-bg-info');
+        }
+      target.classList.add('text-bg-info'); // You can adjust the styling as needed
+    }
   };
   
+  const handleCloseProfile = () => {
+    target.classList.remove('text-bg-info');
+    setSelectedProfile(null);
+    setTarget(null);
+    setShowOverlay(false);
+  };
 
   return (
-    
+    <div className='bg-info-subtle'>
       <Container>
+       <h2>Users</h2>
         <Row>
           <Col xs="auto" md={4} className=''>
             {data.map((items, index) => (
@@ -83,11 +110,11 @@ function App() {
               />
             ))}
           </Col>
-        </Row>
+        
         <Overlay target={target} show={showOverlay} placement="right">
           {({ placement, arrowProps, show: _show, popper, ...props }) => (
             <div
-               className="WOW bounceInRight" data-wow-delay="1.5s"
+               className="profile-card text-bg-info" data-wow-delay="1.5s"
               {...props}
               style={{
                 ...props.style,
@@ -97,8 +124,9 @@ function App() {
                 padding: '10px',
                 zIndex: 1000,
                 maxWidth: '600px',
-                minWidth: '300px', // Adjust width as necessary
-                marginLeft: '10px', // Ensure some space from the target element
+                minWidth: '300px',
+                marginLeft: '10px',
+                position: 'absolute'    
               }}
             >
               {selectedProfile && (
@@ -128,6 +156,11 @@ function App() {
                           <strong>Job Title:</strong><p> {selectedProfile.jobTitle  || 'No data to show'}</p>
                           </Col>
                         
+                          <Row className='mt-3'>
+                            <Col className='text-end'>
+                              <Button variant="secondary" onClick={handleCloseProfile}>Close</Button>
+                            </Col>
+                    </Row>
                       </Row>
                     </Container>
                   </Card.Body>
@@ -136,7 +169,9 @@ function App() {
             </div>
           )}
         </Overlay>
+        </Row>
       </Container>
+      </div>
   );
 }
 
